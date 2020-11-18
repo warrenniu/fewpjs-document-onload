2ac6921243272ed3b03fb7b79b6592a438c1433d
warrenniu <warrenniu@gmail.com>
2020-11-17 22:18:01 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const paragraph = document.querySelector('#text')
+document.addEventListener("DOMContentLoaded", function() {
+    paragraph.textContent = "This is really cool!"
+  });
+
